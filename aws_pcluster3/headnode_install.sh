c154a7fe20b8d030a1a776932da82126c6d337ca
Brian Dobbins <bdobbins@gmail.com>
2022-02-16 02:59:45 +0000

    Tweaks

--- aws_pcluster3/headnode_install.sh
+++ aws_pcluster3/headnode_install.sh
@@ -20,8 +20,33 @@ echo 'source /opt/intel/oneapi/setvars.sh > /dev/null' > /etc/profile.d/oneapi.s
 
 # Get CESM2.1.4-rc.10
 cd /opt/ncar
-
 git clone -b cesm2.1.4-rc.10 https://github.com/ESCOMP/CESM.git cesm
+cd cesm
+
+
+# Change user limits for ec2-user:
+cat << EOF >> /etc/security/limits.conf
+ec2-user         hard    stack           -1
+ec2-user         soft    stack           -1
+EOF
+
+# Make the scratch/inputdata directory for ec2-user
+mkdir -p /scratch/ec2-user/inputdata
+chown -R ec2-user:ec2-user /scratch/ec2-user
+
+# Create the ~/.bashrc for ec2-user:
+cat << EOF >> /home/ec2-user/.bashrc
+module load libfabric-aws
+export OMP_NUM_THREADS=1
+
+export I_MPI_OFI_LIBRARY_INTERNAL=0
+source /opt/intel/oneapi/setvars.sh --force > /dev/null
+export I_MPI_FABRICS=ofi
+export I_MPI_OFI_PROVIDER=efa
+
+export PATH=${PATH}:/opt/ncar/software/bin
+export I_MPI_PMI_LIBRARY=/opt/slurm/lib/libpmi.so
+EOF
 
 # Add this to our path:
 echo 'export PATH=/opt/ncar/cesm/cime/scripts:${PATH}' > /etc/profile.d/cesm.sh
